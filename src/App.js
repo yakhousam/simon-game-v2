ce2cc3dd4077a46e42cfd8a1af7313125637ab38
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-10-26 10:56:58 +0100

    added play sound on button clicked

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
 import "./App.css";
 import Control from "./components/Control/Control";
+import sounds from "./sounds";
 
-const initialState = {
-  
-};
+const initialState = {};
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "BTN_CLICKED":
-    // return {...state, [action.id]:{...state[action.id], className: "red" }}>
+    case "PLAY_SOUND":
+      return { ...state, sound: action.sound };
 
     default:
       return state;
@@ -18,15 +17,48 @@ const reducer = (state, action) => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const audio = useRef();
+  const playSound = sound => {
+    dispatch({
+      type: "PLAY_SOUND",
+      sound
+    });
+    if (state.sound === sound) {
+      audio.current.currentTime = 0;
+      audio.current.play();
+    }
+  };
   return (
     <div className="grid">
-      <div className='green'></div>
-      <div className='red'></div>
-      <div className='yellow'></div>
-      <div className='blue'></div>
+      <div
+        className="green"
+        onClick={() => {
+          playSound(sounds.sound01);
+        }}
+      ></div>
+      <div
+        className="red"
+        onClick={() => {
+          playSound(sounds.sound02);
+        }}
+      ></div>
+      <div
+        className="yellow"
+        onClick={() => {
+          playSound(sounds.sound03);
+        }}
+      ></div>
+      <div
+        className="blue"
+        onClick={() => {
+          playSound(sounds.sound04);
+        }}
+      ></div>
       <Control />
       <div className="sp-vertical"></div>
       <div className="sp-horizontal"></div>
+
+      <audio src={state.sound} ref={audio} autoPlay></audio>
     </div>
   );
 }
---
+++ src/sounds/index.js
@@ -0,0 +1,13 @@
+import sound01 from "./simonSound1.mp3";
+import sound02 from "./simonSound2.mp3";
+import sound03 from "./simonSound3.mp3";
+import sound04 from "./simonSound4.mp3";
+
+const sounds = {
+  sound01,
+  sound02,
+  sound03,
+  sound04
+};
+
+export default sounds;
