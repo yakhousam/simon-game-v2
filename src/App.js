4695abb7876e98c9821832f0111dce8d1fd5ac6e
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-10-28 11:54:02 +0100

    refactored the code each button has its audio element and added wrong buzzer

--- src/App.js
+++ src/App.js
@@ -3,33 +3,77 @@ import "./App.css";
 // import Control from "./components/Control/Control";
 import sounds from "./sounds";
 
+const buttons = ["btnGreen", "btnRed", "btnYellow", "btnBlue", "wrong"];
+
 const initialState = {
-  playList: [sounds[Math.floor(Math.random() * 4)]],
+  btnGreen: {
+    clicked: false,
+    classClicked: "green-clicked",
+    soundEffect: sounds[0]
+  },
+  btnRed: {
+    clicked: false,
+    classClicked: "red-clicked",
+    soundEffect: sounds[1]
+  },
+  btnYellow: {
+    clicked: false,
+    classClicked: "yellow-clicked",
+    soundEffect: sounds[2]
+  },
+  btnBlue: {
+    clicked: false,
+    classClicked: "blue-clicked",
+    soundEffect: sounds[3]
+  },
+  playList: [buttons[Math.floor(Math.random() * 4)]],
   userPlayList: [],
   start: false,
   reset: false,
-  isPlaying: false
+  isPlaying: false,
+  displayer: "-:-",
+  cursor: 0,
+  simonIsBusy: false
 };
 
 const reducer = (state, action) => {
-  console.log("dispatch =", action.type)
+  // console.log("state =", state)
+  console.log("dispatch =", action.type, action.btn || "", action.text || "");
   switch (action.type) {
     case "PLAY_SOUND":
       return { ...state, sound: action.sound };
     case "ADD_PLAYLIST":
-      return { ...state, playList: [...state.playList, action.sound] };
-    case "ADD_USER_PLAYLIST":
-      return { ...state, userPlayList: [...state.userPlayList, action.sound] };
-    case "REST_SOUND_FILE":
-      return { ...state, sound: action.sound };
+      return {
+        ...state,
+        playList: [...state.playList, action.btn]
+      };
     case "IS_PLAYING":
       return { ...state, isPlaying: true };
     case "IS_NOT_PLAYING":
-      return { ...state, isPlaying: false };
+      return { ...state, isPlaying: false, displayer: "-:-" };
     case "START":
       return { ...state, start: !state.start };
     case "RESET":
-      return { ...initialState,  playList: [sounds[Math.floor(Math.random() * 4)]] };
+      return {
+        ...initialState,
+        playList: [buttons[Math.floor(Math.random() * 4)]]
+      };
+    case "INC_CURSOR":
+      return { ...state, cursor: state.cursor + 1 };
+    case "RESET_CURSOR":
+      return { ...state, cursor: 0 };
+    case "SET_DISPLAYER":
+      return { ...state, displayer: action.text };
+    case "BTN_PLAY":
+      return {
+        ...state,
+        [action.btn]: { ...state[action.btn], clicked: true }
+      };
+    case "BTN_STOP":
+      return {
+        ...state,
+        [action.btn]: { ...state[action.btn], clicked: false }
+      };
 
     default:
       return state;
@@ -38,91 +82,162 @@ const reducer = (state, action) => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const audio = useRef();
+  const audioBtnGreen = useRef();
+  const audioBtnRed = useRef();
+  const audioBtnYellow = useRef();
+  const audioBtnBlue = useRef();
+  const audioWorng = useRef();
 
-  const playSound = sound => {
-    dispatch({
-      type: "PLAY_SOUND",
-      sound
-    });
-    dispatch({ type: "IS_PLAYING" });
-    if (state.sound === sound) {
-      audio.current.currentTime = 0;
-      audio.current.play();
+  const playBtn = btn => {
+    if (btn === "btnGreen") {
+      audioBtnGreen.current.currentTime = 0;
+      audioBtnGreen.current.play();
     }
-  };
-  const addUserPlayList = sound => {
-    dispatch({
-      type: "ADD_USER_PLAYLIST",
-      sound
-    });
-  };
+    if (btn === "btnRed") {
+      audioBtnRed.current.currentTime = 0;
+      audioBtnRed.current.play();
+    }
+    if (btn === "btnYellow") {
+      audioBtnYellow.current.currentTime = 0;
+      audioBtnYellow.current.play();
+    }
+    if (btn === "btnBlue") {
+      audioBtnBlue.current.currentTime = 0;
+      audioBtnBlue.current.play();
+    }
+    if (btn === "wrong") {
+      audioWorng.current.currentTime = 0;
+      audioWorng.current.play();
+    }
 
-  const playPlayList = () => {
+    // else{
+    //   dispatch({
+    //     type: "PLAY_SOUND",
+    //     sound
+    //   });
+    //   if (!state.isPlaying) {
+    //     dispatch({ type: "IS_PLAYING" });
+    //   }
+    //   if (state.sound === sound) {
+    //     audio.current.currentTime = 0;
+    //     audio.current.play();
+    //   }
+    // }
+  };
+  // const addUserPlayList = sound => {
+  //   dispatch({
+  //     type: "ADD_USER_PLAYLIST",
+  //     sound
+  //   });
+  // };
+
+  const playPlayList = newBtn => {
+    const list = newBtn ? [...state.playList, newBtn] : [...state.playList];
+    console.log("play playlist----", list);
+    dispatch({ type: "IS_PLAYING" });
+    dispatch({ type: "RESET_CURSOR" });
     setTimeout(() => {
-      state.playList.forEach((sound, i) => {
-        setTimeout(() => {
-        playSound(sound)
-        }, 1000 * (i + 1));
-      });
+    list.forEach((btn, i) => {
+      setTimeout(() => {
+        dispatch({
+          type: "BTN_PLAY",
+          btn
+        });
+        dispatch({
+          type: "SET_DISPLAYER",
+          text: list.length - i
+        });
+        playBtn(btn);
+      }, 1000 * (i + 1));
+    });
+    setTimeout(() => {
+      dispatch({ type: "IS_NOT_PLAYING" });
+    }, 1000 * (list.length + 2));
     }, 1000);
   };
 
-  const addPlayList = sound => {
-    dispatch({
-      type: "ADD_PLAYLIST",
-      sound: state[sound].sound
-    });
+  const checkList = btn => {
+    if (state.cursor > state.playList.length) return;
+    if (state.playList[state.cursor] === btn) {
+      console.log("user click --------");
+      playBtn(btn);
+      dispatch({ type: "INC_CURSOR" });
+      if (state.cursor === state.playList.length - 1) {
+        const newBtn = buttons[Math.floor(Math.random() * 4)];
+        dispatch({ type: "ADD_PLAYLIST", btn: newBtn });
+        playPlayList(newBtn);
+      }
+    } else {
+      dispatch({ type: "SET_DISPLAYER", text: "WRONG" });
+      playBtn("wrong");
+      dispatch({
+        type: "BTN_STOP",
+        btn
+      });
+      dispatch({ type: "RESET_CURSOR" });
+      playPlayList();
+    }
   };
-  
-const checkList = () => {
-  
-}
-
-  
 
   // console.log("state playlist =", state.playList);
   // console.log("sound =", state.sound);
+  const { btnGreen, btnRed, btnYellow, btnBlue } = state;
   return (
     <div className="grid">
       <div
-        className="green"
-        style={sounds[0] === state.sound ? { background: "lime" } : {}}
+        className={`green ${btnGreen.clicked ? btnGreen.classClicked : ""}`}
+        // style={sounds[0] === state.sound ? { background: "lime" } : {}}
         onClick={() => {
-          playSound(sounds[0]);
-          addUserPlayList(sounds[0]);
-          checkList()
+          if (!state.start) return;
+          if (state.isPlaying) return;
+          dispatch({
+            type: "BTN_PLAY",
+            btn: "btnGreen"
+          });
+          checkList("btnGreen");
         }}
       ></div>
       <div
-        className="red"
-        style={sounds[1] === state.sound ? { background: "orange" } : {}}
+        className={`red ${btnRed.clicked ? btnRed.classClicked : ""}`}
         onClick={() => {
-          playSound(sounds[1]);
-          addUserPlayList(sounds[1]);
+          if (!state.start) return;
+          if (state.isPlaying) return;
+          dispatch({
+            type: "BTN_PLAY",
+            btn: "btnRed"
+          });
+          checkList("btnRed");
         }}
       ></div>
       <div
-        className="yellow"
-        style={sounds[2] === state.sound ? { background: "yellow" } : {}}
+        className={`yellow ${btnYellow.clicked ? btnYellow.classClicked : ""}`}
         onClick={() => {
-          playSound(sounds[2]);
-          addUserPlayList(sounds[2]);
+          if (!state.start) return;
+          if (state.isPlaying) return;
+          dispatch({
+            type: "BTN_PLAY",
+            btn: "btnYellow"
+          });
+          checkList("btnYellow");
         }}
       ></div>
       <div
-        className="blue"
-        style={sounds[3] === state.sound ? { background: "skyblue" } : {}}
+        className={`blue ${btnBlue.clicked ? btnBlue.classClicked : ""}`}
         onClick={() => {
-          playSound(sounds[3]);
-          addUserPlayList(sounds[3]);
+          if (!state.start) return;
+          if (state.isPlaying) return;
+          dispatch({
+            type: "BTN_PLAY",
+            btn: "btnBlue"
+          });
+          checkList("btnBlue");
         }}
       ></div>
 
       <div className="control">
         <h1 className="title">Simon</h1>
         <div className="monitor">
-          <span>Good</span>
+          <span>{state.displayer}</span>
         </div>
         <div className="btn-wrap">
           <div
@@ -134,8 +249,7 @@ const checkList = () => {
                   type: "START"
                 });
                 playPlayList();
-                // const sound = Math.floor(Math.random() * 4);
-                // addPlayList(sound);
+                const sound = Math.floor(Math.random() * 4);
               } else {
                 dispatch({ type: "RESET" });
               }
@@ -151,17 +265,66 @@ const checkList = () => {
       <div className="sp-vertical"></div>
       <div className="sp-horizontal"></div>
 
-      <audio src={state.sound} ref={audio} autoPlay onEnded={() => {
-        // console.log('audio ended')
-        dispatch({
-          type: "REST_SOUND_FILE",
-          sound:undefined
-        })
-        dispatch({
-          type: "IS_NOT_PLAYING"
-        })
-      }
-      } ></audio>
+      <audio
+        src={state.btnGreen.soundEffect}
+        ref={audioBtnGreen}
+        onEnded={() => {
+          // console.log('audio ended')
+
+          dispatch({
+            type: "BTN_STOP",
+            btn: "btnGreen"
+          });
+        }}
+      ></audio>
+      <audio
+        src={state.btnRed.soundEffect}
+        ref={audioBtnRed}
+        onEnded={() => {
+          // console.log('audio ended')
+
+          dispatch({
+            type: "BTN_STOP",
+            btn: "btnRed"
+          });
+        }}
+      ></audio>
+      <audio
+        src={state.btnYellow.soundEffect}
+        ref={audioBtnYellow}
+        onEnded={() => {
+          // console.log('audio ended')
+
+          dispatch({
+            type: "BTN_STOP",
+            btn: "btnYellow"
+          });
+        }}
+      ></audio>
+      <audio
+        src={state.btnBlue.soundEffect}
+        ref={audioBtnBlue}
+        onEnded={() => {
+          // console.log('audio ended')
+
+          dispatch({
+            type: "BTN_STOP",
+            btn: "btnBlue"
+          });
+        }}
+      ></audio>
+      <audio
+        src={sounds[4]}
+        ref={audioWorng}
+        onEnded={() => {
+          // console.log('audio ended')
+
+          dispatch({
+            type: "BTN_STOP",
+            btn: "wrong"
+          });
+        }}
+      ></audio>
     </div>
   );
 }
--- src/sounds/index.js
+++ src/sounds/index.js
@@ -2,12 +2,14 @@ import sound01 from "./simonSound1.mp3";
 import sound02 from "./simonSound2.mp3";
 import sound03 from "./simonSound3.mp3";
 import sound04 from "./simonSound4.mp3";
+import wrong from "./wrong-answer-sound-effect.mp3"
 
 const sounds = [
   sound01,
   sound02,
   sound03,
-  sound04
+  sound04,
+  wrong
 ];
 
 export default sounds;
