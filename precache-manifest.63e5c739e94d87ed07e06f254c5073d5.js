012d881e195cd3a68f7e20792b0f6953a7c26993
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-22 10:42:20 +0100

    Updates

---
+++ precache-manifest.63e5c739e94d87ed07e06f254c5073d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be9e9f27ebcccdd35ffced18bed3ea5",
+    "url": "/simon-game-v2/index.html"
+  },
+  {
+    "revision": "477ed1062aa6f20e9449",
+    "url": "/simon-game-v2/static/css/main.04f89044.chunk.css"
+  },
+  {
+    "revision": "191b73d373d32ba507dd",
+    "url": "/simon-game-v2/static/js/2.46c77aac.chunk.js"
+  },
+  {
+    "revision": "477ed1062aa6f20e9449",
+    "url": "/simon-game-v2/static/js/main.4da5ecff.chunk.js"
+  },
+  {
+    "revision": "d821e419fa3af776f0a2",
+    "url": "/simon-game-v2/static/js/runtime-main.550de018.js"
+  },
+  {
+    "revision": "319c77102049da3622a954acd189f1eb",
+    "url": "/simon-game-v2/static/media/simonSound1.319c7710.mp3"
+  },
+  {
+    "revision": "c3c86e71f309dde162a2e9b52cbc702f",
+    "url": "/simon-game-v2/static/media/simonSound2.c3c86e71.mp3"
+  },
+  {
+    "revision": "f3c18b98bab33c0fbedde9cb67bdfcf2",
+    "url": "/simon-game-v2/static/media/simonSound3.f3c18b98.mp3"
+  },
+  {
+    "revision": "f4a912ee52e2c884a80499208c5568cc",
+    "url": "/simon-game-v2/static/media/simonSound4.f4a912ee.mp3"
+  },
+  {
+    "revision": "093eed4f4091d0792b60b33a1d98448c",
+    "url": "/simon-game-v2/static/media/wrong-answer-sound-effect.093eed4f.mp3"
+  }
+]);
